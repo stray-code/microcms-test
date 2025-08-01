39bbb6e6ca56692115028a6627a95212c49be7bd
stray-code <abc1007020@gmail.com>
2025-08-01 23:01:56 +0900

    お知らせの個別ページ作成

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,1 +1,33 @@
-<template>about</template>
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+import { onMounted, ref } from 'vue'
+import type { News } from '@/types'
+import { cmsClient } from '@/utils'
+
+const router = useRouter()
+
+const news = ref<News | null>(null)
+
+onMounted(async () => {
+  const id = router.currentRoute.value.params.id
+
+  try {
+    const response = await cmsClient.get({
+      endpoint: 'news',
+      contentId: id.toString(),
+    })
+
+    news.value = response
+  } catch (error) {
+    console.log(error)
+  }
+})
+</script>
+
+<template>
+  <div v-if="news">
+    <h3>{{ news.title }}</h3>
+    <time>{{ new Date(news.publishedAt).toLocaleDateString() }}</time>
+    <p v-html="news.content"></p>
+  </div>
+</template>
