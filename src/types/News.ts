984d042dbd5789c64ffb4f0a373944b3e9462d01
stray-code <abc1007020@gmail.com>
2025-08-01 22:43:25 +0900

    お知らせを取得して表示

---
+++ src/types/News.ts
@@ -0,0 +1,12 @@
+import type { Category } from "@/types"
+
+export type News = {
+  id: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+  revisedAt: string
+  title: string
+  content: string
+  category: Category
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { News } from '@/types'
+import { onMounted, ref } from 'vue'
+import { RouterLink } from 'vue-router'
+
+const newsList = ref<News[]>([])
+
+onMounted(async () => {
+  const response = await fetch('https://hke0ar7pmq.microcms.io/api/v1/news', {
+    headers: {
+      'X-MICROCMS-API-KEY': import.meta.env.VITE_MICROCMS_API_KEY,
+    },
+  })
+
+  if (response.ok) {
+    const data = await response.json()
+    newsList.value = data.contents.reverse()
+  }
+})
+</script>
 
 <template>
-  <main>home</main>
+  <main>
+    <div v-for="news in newsList">
+      <h3>
+        <RouterLink :to="`news/${news.id}`">{{ news.title }}</RouterLink>
+      </h3>
+      <time>{{ new Date(news.publishedAt).toLocaleDateString() }}</time>
+    </div>
+  </main>
 </template>
