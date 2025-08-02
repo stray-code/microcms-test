4da70e13f835ee84b7c595c64c5cf076f17e43c2
stray-code <abc1007020@gmail.com>
2025-08-03 00:31:43 +0900

    お知らせ検索機能を追加

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,7 @@ import { RouterLink, useRouter } from 'vue-router'
 const router = useRouter()
 
 const newsList = ref<News[]>([])
+const keyword = ref('')
 
 const fetchNews = async () => {
   const query = router.currentRoute.value.query
@@ -18,7 +19,11 @@ const fetchNews = async () => {
   try {
     const response = await cmsClient.get({
       endpoint: 'news',
-      queries: { orders: '-publishedAt', filters: category },
+      queries: {
+        orders: '-publishedAt',
+        filters: category,
+        q: query.keyword?.toString(),
+      },
     })
 
     newsList.value = response.contents
@@ -35,7 +40,24 @@ watch(() => router.currentRoute.value.query, fetchNews)
 <template>
   <main>
     <section>
-      <h2 class="mb-3 text-xl">お知らせ</h2>
+      <div class="flex justify-between">
+        <h2 class="mb-3 text-xl">お知らせ</h2>
+        <form @submit.prevent="$router.push(`?keyword=${keyword}`)">
+          <div class="flex items-center gap-1">
+            <input
+              type="text"
+              v-model="keyword"
+              class="p-1 border-1 border-gray-300 rounded-sm"
+            />
+            <button
+              type="submit"
+              class="bg-blue-500 py-2 px-3 rounded-sm text-white text-sm"
+            >
+              検索
+            </button>
+          </div>
+        </form>
+      </div>
       <div
         v-for="news in newsList"
         :key="news.id"
