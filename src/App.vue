07f1c6d560424fb133cd660bf1c1d08e2e257b87
stray-code <abc1007020@gmail.com>
2025-08-06 17:30:00 +0900

    横幅の最大値設定

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="my-3 px-8 mx-auto">
+  <div class="my-3 px-8 max-w-3xl mx-auto">
     <header class="mb-8">
       <nav class="flex justify-between">
         <RouterLink to="/" class="text-blue-600 text-xl"
